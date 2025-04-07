455d179d41721ba35f67a27191e775ad97900968
Mr-76 <bartmoss77@tutanota.com>
2025-04-07 15:53:27 -0300

    work with remote audio send

--- Servitor.py
+++ Servitor.py
@@ -11,20 +11,27 @@ import torch
 from TTS.api import TTS
 import subprocess
 import speech_recognition as sr
+import socket
 r = sr.Recognizer()
 
-def tts(talk,model_name,output_path):
-    print("Now in tts")
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
-    tts.tts_to_file(
-      text=talk,
-      speaker="Craig Gutsy",
-      language="en",
-      file_path="./audio.wav"
-    )
 
+def receive_file(file_name, client_socket):
+    with open(file_name, 'wb') as file:
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+            file.write(data)
+
+    print(f"File '{file_name}' received successfully.")
+    client_socket.close()
+
+def tts(talk):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('172.22.168.86', 12345))
+    client_socket.sendall(talk.encode('utf-8'))
+    receive_file('audio.wav', client_socket)
 
     try:
         command = 'sox audio.wav robot_voice.wav overdrive 10 echo 0.8 0.88 60 0.4 chorus 0.7 0.9 55 0.4 0.25 2 -s'
@@ -33,40 +40,29 @@ def tts(talk,model_name,output_path):
     except subprocess.CalledProcessError as e:
         print(f"Error running TTS: {e}")
 
-
     playsound('robot_voice.wav')
     os.remove('audio.wav')
-    print("now exiting")
-
-
 
 def stt():
     while(1):
-        with sr.Microphone() as source:
-            print("Say something!")
-            audio = r.listen(source)
-        try:
-            phrase = r.recognize_google(audio)
-            print("Google Speech Recognition thinks you said " + phrase)
-            if ("Migos" in phrase or "migos" in phrase):
-                phrase = phrase.replace('Migos', '')
-                result = ollama_talker(phrase)
-                model_name="tts_models/multilingual/multi-dataset/xtts_v2",
-                tts(result,model_name,"./audio.wav")
-                time.sleep(1)
-
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+       # with sr.Microphone() as source:
+       #     print("Say something!")
+       #     #audio = r.listen(source)
+       #     audio = "speak about the chaos gods"
+       # try:
+            #phrase = r.recognize_google(audio)
+        phrase = "Magos speak about the chaos gods"
+        result = ollama_talker(phrase)
+        tts(result)
         time.sleep(1)
+
 
 
 def ollama_talker(phrase):
     print("this was the phrase ")
     print(phrase)
     client = Client(
-            host='http://192.168.0.12:11434',
+            host='http://172.22.168.86:11434',
             headers={'x-some-header': 'some-value'}
 )
     system_prompt = "You are now a warhammer 40k MAGOs, use the same persolnality as one showing curiosity for cience in all manners ,also only need short reponses, you are like a magos from a library from teh imperium and answeer all questioes "
